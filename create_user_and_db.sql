3767aa7284b795aeeda15c1cca1d4231e3dea02d
Gilyoung <roadzero@kodebox.io>
2018-12-19 16:22:26 +0900

    Modify creating user script

--- create_user_and_db.sql
+++ create_user_and_db.sql
@@ -1,3 +1,5 @@
-CREATE DATABASE "codechain-indexer";
+CREATE DATABASE "codechain-indexer-dev";
+CREATE DATABASE "codechain-indexer-test";
 CREATE USER "user" WITH ENCRYPTED PASSWORD 'password';
-GRANT ALL PRIVILEGES ON DATABASE "codechain-indexer" TO "user";
+GRANT ALL PRIVILEGES ON DATABASE "codechain-indexer-dev" TO "user";
+GRANT ALL PRIVILEGES ON DATABASE "codechain-indexer-test" TO "user";
