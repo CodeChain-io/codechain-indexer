4c49a7e03679b6e12eb82f1f1a5f773ae01a69dc
Gilyoung <roadzero@kodebox.io>
2018-12-19 17:47:14 +0900

    Add the sequelize-cli to the package

--- database.js
+++ database.js
@@ -1,7 +1,8 @@
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 const { pg, sequelize } = require("config");
 const _ = require("lodash");
 
 const config = _.assign({}, pg, { username: pg.user }, sequelize);
-const mode = process.env.NODE_ENV || "dev";
+const mode = process.env.NODE_ENV;
 
 module.exports[mode] = config;
