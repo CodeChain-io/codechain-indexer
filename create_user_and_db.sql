405fc25dabee3c8e75f5b74937e40f0dfad150f3
Park Juhyung <jh@kodebox.io>
2019-01-18 18:35:30 +0900

    Make the "user" is the owner of database
    To drop and recreate database in integrationtests, the "user" should
    be owner of the database.

--- create_user_and_db.sql
+++ create_user_and_db.sql
@@ -1,5 +1,7 @@
-CREATE DATABASE "codechain-indexer-dev";
-CREATE DATABASE "codechain-indexer-test";
 CREATE USER "user" WITH ENCRYPTED PASSWORD 'password';
+CREATE DATABASE "codechain-indexer-dev" WITH OWNER "user";
+CREATE DATABASE "codechain-indexer-test" WITH OWNER "user";
+CREATE DATABASE "codechain-indexer-test-int" WITH OWNER "user";
 GRANT ALL PRIVILEGES ON DATABASE "codechain-indexer-dev" TO "user";
 GRANT ALL PRIVILEGES ON DATABASE "codechain-indexer-test" TO "user";
+GRANT ALL PRIVILEGES ON DATABASE "codechain-indexer-test-int" TO "user";
