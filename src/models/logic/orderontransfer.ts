2f4f259338c92ffa9d8ff5cfe84bac3bae0a9797
Seonpyo Kim <tomandjerry@kodebox.io>
2019-01-29 20:21:32 +0900

    Implement models and logics for order

---
+++ src/models/logic/orderontransfer.ts
@@ -0,0 +1,50 @@
+import { OrderOnTransfer } from "codechain-sdk/lib/core/classes";
+import { H256 } from "codechain-sdk/lib/core/classes";
+import models from "..";
+import * as Exception from "../../exception";
+import { OrderOnTransferInstance } from "../orderontransfer";
+import { createOrder } from "./order";
+
+export async function createOrderOnTransfer(
+    transactionHash: string,
+    orderOnTransfer: OrderOnTransfer,
+    networkId: string
+): Promise<OrderOnTransferInstance> {
+    let orderOnTransferInstance: OrderOnTransferInstance;
+    try {
+        orderOnTransferInstance = await models.OrderOnTransfer.create({
+            transactionHash,
+            spentQuantity: orderOnTransfer.spentQuantity.value.toString(10),
+            inputIndices: orderOnTransfer.inputIndices,
+            outputIndices: orderOnTransfer.outputIndices
+        });
+
+        const order = orderOnTransfer.order;
+        if (await orderNotExist(order.hash())) {
+            await createOrder(
+                transactionHash,
+                orderOnTransfer.order,
+                networkId
+            );
+        }
+    } catch (err) {
+        console.error(err);
+        throw Exception.DBError;
+    }
+
+    return orderOnTransferInstance;
+}
+
+async function orderNotExist(orderHash: H256) {
+    try {
+        const order = await models.Order.findOne({
+            where: {
+                orderHash: orderHash.value
+            }
+        });
+        return order === null;
+    } catch (err) {
+        console.error(err);
+        throw Exception.DBError;
+    }
+}
---
+++ src/models/order.ts
@@ -0,0 +1,125 @@
+import * as Sequelize from "sequelize";
+import { AssetOutPointAttribute } from "./assettransferinput";
+
+export interface OrderAttribute {
+    id?: string;
+    orderHash: string;
+    transactionHash: string;
+
+    assetTypeFrom: string;
+    assetTypeTo: string;
+    assetTypeFee: string;
+    shardIdFrom: number;
+    shardIdTo: number;
+    shardIdFee: number;
+    assetQuantityFrom: string;
+    assetQuantityTo: string;
+    assetQuantityFee: string;
+
+    originOutputs: AssetOutPointAttribute[];
+    expiration: string;
+    lockScriptHashFrom: string;
+    parametersFrom: string[];
+    lockScriptHashFee: string;
+    parametersFee: string[];
+}
+
+export interface OrderInstance extends Sequelize.Instance<OrderAttribute> {}
+
+export default (
+    sequelize: Sequelize.Sequelize,
+    DataTypes: Sequelize.DataTypes
+) => {
+    const Order = sequelize.define(
+        "Order",
+        {
+            orderHash: {
+                allowNull: false,
+                primaryKey: true,
+                type: DataTypes.STRING
+            },
+            TransactionHash: {
+                allowNull: false,
+                type: DataTypes.STRING,
+                onDelete: "CASCADE",
+                references: {
+                    model: "Transactions",
+                    key: "hash"
+                }
+            },
+            assetTypeFrom: {
+                allowNull: false,
+                type: DataTypes.STRING
+            },
+            assetTypeTo: {
+                allowNull: false,
+                type: DataTypes.STRING
+            },
+            assetTypeFee: {
+                allowNull: false,
+                type: DataTypes.STRING
+            },
+            shardIdFrom: {
+                allowNull: false,
+                type: DataTypes.INTEGER
+            },
+            shardIdTo: {
+                allowNull: false,
+                type: DataTypes.INTEGER
+            },
+            shardIdFee: {
+                allowNull: false,
+                type: DataTypes.INTEGER
+            },
+            assetQuantityFrom: {
+                allowNull: false,
+                type: DataTypes.STRING
+            },
+            assetQuantityTo: {
+                allowNull: false,
+                type: DataTypes.STRING
+            },
+            assetQuantityFee: {
+                allowNull: false,
+                type: DataTypes.STRING
+            },
+            originOutputs: {
+                allowNull: false,
+                type: DataTypes.JSONB
+            },
+            expiration: {
+                allowNull: false,
+                type: DataTypes.STRING
+            },
+            lockScriptHashFrom: {
+                allowNull: false,
+                type: DataTypes.STRING
+            },
+            paramtersFrom: {
+                allowNull: false,
+                type: DataTypes.JSONB
+            },
+            lockScriptHashFee: {
+                allowNull: false,
+                type: DataTypes.STRING
+            },
+            parametersFee: {
+                allowNull: false,
+                type: DataTypes.JSONB
+            },
+            createdAt: {
+                allowNull: false,
+                type: DataTypes.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: DataTypes.DATE
+            }
+        },
+        {}
+    );
+    Order.associate = () => {
+        // associations can be defined here
+    };
+    return Order;
+};
