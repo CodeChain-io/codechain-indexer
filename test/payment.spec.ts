7edd4ab2f474150926006948b9c452c570fa3c38
Seulgi Kim <dev@seulgi.kim>
2019-02-03 01:01:00 +0900

    Fix the bug that occurred when the payment quantity is large

--- test/payment.spec.ts
+++ test/payment.spec.ts
@@ -37,3 +37,49 @@ test("Create payment block", async done => {
     );
     done();
 });
+
+test("Pay large amount", async done => {
+    const ACCOUNT_SECRET =
+        "ede1d4ccb4ec9a8bbbae9a13db3f4a7b56ea04189be86ac3a6a439d9a0a1addd";
+    const ACCOUNT_ADDRESS =
+        "tccq9h7vnl68frvqapzv3tujrxtxtwqdnxw6yamrrgd";
+    const seq = await Helper.sdk.rpc.chain.getSeq(ACCOUNT_ADDRESS);
+    const tx = Helper.sdk.core.createPayTransaction({
+        recipient: "tccqxv9y4cw0jwphhu65tn4605wadyd2sxu5yezqghw",
+        quantity: "0x0186a0"
+    });
+    const hash = await Helper.sdk.rpc.chain.sendSignedTransaction(
+        tx.sign({
+            secret: ACCOUNT_SECRET,
+            fee: 10,
+            seq
+        })
+    );
+    const invoice = (await Helper.sdk.rpc.chain.getInvoice(hash, {
+        timeout: 300 * 1000
+    }))!;
+    expect(invoice).toBeTruthy();
+    expect(invoice.success).toEqual(true);
+
+    const paymentBlockNumber = await Helper.sdk.rpc.chain.getBestBlockNumber();
+    const paymentBlock = await Helper.sdk.rpc.chain.getBlock(
+        paymentBlockNumber
+    );
+    expect(paymentBlock).toBeTruthy();
+    const miningRewardResponse = await Helper.sdk.rpc.sendRpcRequest(
+        "chain_getMiningReward",
+        [paymentBlockNumber]
+    );
+
+    expect(miningRewardResponse).toBeTruthy();
+    await BlockModel.createBlock(paymentBlock!, {
+        miningReward: new U64("1000"),
+        invoices: [{ invoice: true}]
+    });
+    const lastBlockInstance = await BlockModel.getLatestBlock();
+    expect(lastBlockInstance).toBeTruthy();
+    expect(lastBlockInstance!.get({ plain: true }).number).toEqual(
+        paymentBlock!.number
+    );
+    done();
+});
