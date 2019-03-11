dfb57390b751ed92f35f23380fba8f86dc29dcbc
Seongjae Choi <cubist@kodebox.io>
2019-03-11 15:10:44 +0900

    Format files in script/ and test/

--- test/pending-tx.spec.ts
+++ test/pending-tx.spec.ts
@@ -18,41 +18,37 @@ afterAll(async done => {
     done();
 });
 
-test(
-    "Check pending transactions",
-    async done => {
-        await Helper.sdk.rpc.devel.stopSealing();
-        p = Helper.runExample("send-signed-tx");
-        await waitForSecond(2);
-        await Helper.worker.sync();
+test("Check pending transactions", async done => {
+    await Helper.sdk.rpc.devel.stopSealing();
+    p = Helper.runExample("send-signed-tx");
+    await waitForSecond(2);
+    await Helper.worker.sync();
 
-        const pendingTransactionsInst = await TxModel.getPendingTransactions({});
-        expect(pendingTransactionsInst.length).toEqual(1);
+    const pendingTransactionsInst = await TxModel.getPendingTransactions({});
+    expect(pendingTransactionsInst.length).toEqual(1);
 
-        const pendingTx = await pendingTransactionsInst![0]!.get();
-        expect(pendingTx.isPending).toEqual(true);
+    const pendingTx = await pendingTransactionsInst![0]!.get();
+    expect(pendingTx.isPending).toEqual(true);
 
-        await Helper.sdk.rpc.devel.startSealing();
-        while ((await Helper.sdk.rpc.chain.getPendingTransactions()).length !== 0) {
-            await waitForSecond(1);
-            console.log("waiting ...");
-        }
-        await Helper.worker.sync();
+    await Helper.sdk.rpc.devel.startSealing();
+    while ((await Helper.sdk.rpc.chain.getPendingTransactions()).length !== 0) {
+        await waitForSecond(1);
+        console.log("waiting ...");
+    }
+    await Helper.worker.sync();
 
-        const newPendingTransactions = await TxModel.getPendingTransactions({});
-        expect(newPendingTransactions.length).toEqual(0);
+    const newPendingTransactions = await TxModel.getPendingTransactions({});
+    expect(newPendingTransactions.length).toEqual(0);
 
-        const indexedTransactionInst = await TxModel.getByHash(
-            new H256(pendingTx.hash)
-        );
-        expect(indexedTransactionInst).toBeTruthy();
-        const indexedTransaction = indexedTransactionInst!.get();
-        expect(indexedTransaction.isPending).toEqual(false);
+    const indexedTransactionInst = await TxModel.getByHash(
+        new H256(pendingTx.hash)
+    );
+    expect(indexedTransactionInst).toBeTruthy();
+    const indexedTransaction = indexedTransactionInst!.get();
+    expect(indexedTransaction.isPending).toEqual(false);
 
-        done();
-    },
-    25000
-);
+    done();
+}, 25000);
 
 function waitForSecond(second: number) {
     return new Promise(resolve => {
