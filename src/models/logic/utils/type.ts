9c1bb39a72ac90c5bda50a48749d892a5243e368
JinGyeong Jeong <jjg@kodebox.io>
2019-03-25 14:07:27 +0900

    Remove the unused util function

--- src/models/logic/utils/type.ts
+++
@@ -1,6 +0,0 @@
-import * as _ from "lodash";
-import { BlockAttribute } from "../../block";
-
-export function getTransactions(block: BlockAttribute) {
-    return _.chain(block.transactions).value();
-}
