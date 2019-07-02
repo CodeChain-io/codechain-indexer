a4a7d014eab21899e5b71c569ea84cf2c627c543
JinGyeong Jeong <jjg@kodebox.io>
2019-07-02 21:03:56 +0900

    Fix getSuccessfulByTracker()

--- src/models/logic/transaction.ts
+++ src/models/logic/transaction.ts
@@ -641,7 +641,8 @@ export async function getSuccessfulByTracker(
     try {
         return await models.Transaction.findOne({
             where: {
-                tracker: strip0xPrefix(tracker.toString())
+                tracker: strip0xPrefix(tracker.toString()),
+                isPending: false
             },
             transaction: options.transaction
         });
